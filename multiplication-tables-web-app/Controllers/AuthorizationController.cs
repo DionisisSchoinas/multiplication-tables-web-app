d44cc6112cac6ca355e2af9b6a53e7e1af7ba1bc
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-06-23 00:58:01 +0300

    Register and login
    - Needs better checks and required

--- multiplication-tables-web-app/Controllers/AuthorizationController.cs
+++ multiplication-tables-web-app/Controllers/AuthorizationController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using multiplication_tables_web_app.Models;
 
 namespace multiplication_tables_web_app.Controllers
 {
     public class AuthorizationController : Controller
     {
+        private SchoolDBEntities db = new SchoolDBEntities();
+
         // GET: Authorization
         public ActionResult Index()
         {
@@ -16,8 +19,48 @@ namespace multiplication_tables_web_app.Controllers
 
         // POST: Authorization
         [HttpPost]
-        public ActionResult Index(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Index(User user_login)
         {
+            var user = db.Users.Where(u => u.Username.Equals(user_login.Username) && u.Password.Equals(user_login.Password));
+            if (user.Count() != 0)
+            {
+                var logged_user = user.First();
+                var teacher = db.Teachers.Where(u => u.UserID.Equals(logged_user.UserID));
+                if (teacher.Count() != 0)
+                {
+                    Session["is_teacher"] = true;
+                    Session["teacher_id"] = teacher.First().TeacherID;
+                    Session["name"] = logged_user.Name;
+                    return Redirect("/");
+                }
+
+                var student = db.Students.Where(u => u.UserID.Equals(logged_user.UserID));
+                if (student.Count() != 0)
+                {
+                    Session["is_teacher"] = false;
+                    Session["student_id"] = student.First().StudentID;
+                    Session["name"] = logged_user.Name;
+                    return Redirect("/");
+                }
+
+                ViewBag.error = "Unknown error";
+                return View();
+            }
+            else
+            {
+                ViewBag.error = "Invalid credentials";
+                return View();
+            }
+        }
+
+        // GET: Authorization
+        public ActionResult Logout()
+        {
+            Session.Remove("is_teacher");
+            Session.Remove("teacher_id");
+            Session.Remove("student_id");
+            Session.Remove("name");
             return Redirect("/");
         }
 
@@ -29,8 +72,30 @@ namespace multiplication_tables_web_app.Controllers
 
         // POST: Authorization/Student
         [HttpPost]
-        public ActionResult Student(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Student(User new_student, string Password_Confirmation)
         {
+            if (new_student.Password != Password_Confirmation)
+            {
+                ViewBag.error = "Password and Password Confirmation must match";
+                return View();
+            }
+
+            if (db.Users.Where(m => m.Username.Equals(new_student.Username)).Count() != 0)
+            {
+                ViewBag.error = "User already exists";
+                return View();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var new_user = db.Users.Add(new_student);
+                var student = new Student();
+                student.UserID = new_user.UserID;
+                db.Students.Add(student);
+                db.SaveChanges();
+            }
+
             return Redirect("/");
         }
 
@@ -42,8 +107,30 @@ namespace multiplication_tables_web_app.Controllers
 
         // POST: Authorization/Teacher
         [HttpPost]
-        public ActionResult Teacher(FormCollection collection)
+        [ValidateAntiForgeryToken]
+        public ActionResult Teacher(User new_teacher, string Password_Confirmation)
         {
+            if (new_teacher.Password != Password_Confirmation)
+            {
+                ViewBag.error = "Password and Password Confirmation must match";
+                return View();
+            }
+
+            if (db.Users.Where(m => m.Username.Equals(new_teacher.Username)).Count() != 0)
+            {
+                ViewBag.error = "User already exists";
+                return View();
+            }
+
+            if (ModelState.IsValid)
+            {
+                var new_user = db.Users.Add(new_teacher);
+                var teacher = new Teacher();
+                teacher.UserID = new_user.UserID;
+                db.Teachers.Add(teacher);
+                db.SaveChanges();
+            }
+
             return Redirect("/");
         }
     }
