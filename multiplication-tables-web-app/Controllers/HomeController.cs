2792f65e61d2a383a565460d3682988054bb7818
Schoinas Dionisis <schinasdionisis@gmail.com>
2021-06-26 18:31:04 +0300

    Added basic theory

--- multiplication-tables-web-app/Controllers/HomeController.cs
+++ multiplication-tables-web-app/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using multiplication_tables_web_app.Models;
 
 namespace multiplication_tables_web_app.Controllers
 {
     public class HomeController : Controller
     {
+        private SchoolDBEntities db = new SchoolDBEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -15,6 +18,9 @@ namespace multiplication_tables_web_app.Controllers
 
         public ActionResult Theory()
         {
+            var testNames = db.TestNames.ToList();
+            testNames.RemoveAt(testNames.Count()-1);
+            ViewBag.testNames = testNames;
             return View();
         }
     }
