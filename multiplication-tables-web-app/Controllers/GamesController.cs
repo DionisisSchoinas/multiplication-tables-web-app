31cbdae909a07c9eef93552b22344b03051a2164
milosmatic1513 <35368267+milosmatic1513@users.noreply.github.com>
2021-07-03 17:10:27 +0300

    Added cloud game

---
+++ multiplication-tables-web-app/Controllers/GamesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace multiplication_tables_web_app.Controllers
+{
+    public class GamesController : Controller
+    {
+        // GET: Games
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CloudGame()
+        {
+            return View();
+        }
+    }
+}
